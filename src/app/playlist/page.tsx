1087f5fd5fef45cffa5b77d0bfb4506cefc98087
HLong <hailongkc1203@gmail.com>
2024-03-17 00:48:16 +0700

    Design App Bar

---
+++ src/app/playlist/page.tsx
@@ -0,0 +1,12 @@
+import AppHeader from "@/components/header/app.header";
+import React from "react";
+
+const PlaylistPage = () => {
+  return (
+    <>
+      <AppHeader />
+    </>
+  );
+};
+
+export default PlaylistPage;
