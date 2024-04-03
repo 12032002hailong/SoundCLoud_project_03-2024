e9b6e61fc679fedcc3f2eab78af563d5b5c1da89
HLong <hailongkc1203@gmail.com>
2024-04-04 03:26:11 +0700

    add Grandients

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -17,10 +17,47 @@ const WaveTrack = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   const optionsMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d")!;
+
+    const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+    gradient.addColorStop(0, "#656666"); // Top color
+    gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
+    gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff"); // White line
+    gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#ffffff"); // White line
+    gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#B1B1B1"); // Bottom color
+    gradient.addColorStop(1, "#B1B1B1");
+
+    const progressGradient = ctx.createLinearGradient(
+      0,
+      0,
+      0,
+      canvas.height * 1.35
+    );
+    progressGradient.addColorStop(0, "#EE772F"); // Top color
+    progressGradient.addColorStop(
+      (canvas.height * 0.7) / canvas.height,
+      "#EB4926"
+    ); // Top color
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 1) / canvas.height,
+      "#ffffff"
+    ); // White line
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 2) / canvas.height,
+      "#ffffff"
+    ); // White line
+    progressGradient.addColorStop(
+      (canvas.height * 0.7 + 3) / canvas.height,
+      "#F6B094"
+    ); // Bottom color
+    progressGradient.addColorStop(1, "#F6B094");
+
     return {
-      waveColor: "rgb(200, 0, 200)",
-      progressColor: "rgb(100, 0, 100)",
-      barWidth: 2,
+      height: 50,
+      waveColor: gradient,
+      progressColor: progressGradient,
+      barWidth: 1.5,
       url: `/api?audio=${fileName}`,
     };
   }, []);
