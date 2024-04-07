a12c1143392ebd88516a25f53301f2e7b917d81f
HLong <hailongkc1203@gmail.com>
2024-04-07 21:11:16 +0700

    layout Music + add tooltip

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useWavesurfer } from "@/utils/customHook";
-import { Button, Container } from "@mui/material";
+import { Button, Container, Tooltip, colors } from "@mui/material";
 import { useSearchParams } from "next/navigation";
 import React, {
   useCallback,
@@ -11,6 +11,8 @@ import React, {
 } from "react";
 import { WaveSurferOptions } from "wavesurfer.js";
 import "./wave.scss";
+import PauseIcon from "@mui/icons-material/Pause";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const WaveTrack = () => {
   const searchParams = useSearchParams();
@@ -21,7 +23,6 @@ const WaveTrack = () => {
   const [duration, setDuration] = useState<string>("0:00");
 
   const optionsMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
-
     let gradient;
     let progressGradient;
 
@@ -29,26 +30,53 @@ const WaveTrack = () => {
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext("2d")!;
 
-      gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1);
+      gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
       gradient.addColorStop(0, "#656666"); // Top color
       gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
-      gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff"); // White line
-      gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#ffffff"); // White line
-      gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#B1B1B1"); // Bottom color
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 1) / canvas.height,
+        "#ffffff"
+      ); // White line
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 2) / canvas.height,
+        "#ffffff"
+      ); // White line
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 3) / canvas.height,
+        "#B1B1B1"
+      ); // Bottom color
       gradient.addColorStop(1, "#B1B1B1");
 
-      progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+      progressGradient = ctx.createLinearGradient(
+        0,
+        0,
+        0,
+        canvas.height * 1.35
+      );
       progressGradient.addColorStop(0, "#EE772F"); // Top color
-      progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, "#EB4926"); // Top color
-      progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, "#ffffff"); // White line
-      progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, "#ffffff"); // White line
-      progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, "#F6B094"); // Bottom color
+      progressGradient.addColorStop(
+        (canvas.height * 0.7) / canvas.height,
+        "#EB4926"
+      ); // Top color
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 1) / canvas.height,
+        "#ffffff"
+      ); // White line
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 2) / canvas.height,
+        "#ffffff"
+      ); // White line
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 3) / canvas.height,
+        "#F6B094"
+      ); // Bottom color
       progressGradient.addColorStop(1, "#F6B094");
     }
     return {
       waveColor: gradient,
       progressColor: progressGradient,
-      barWidth: 1.5,
+      barWidth: 3,
+      height: 100,
       url: `/api?audio=${fileName}`,
     };
   }, []);
@@ -67,18 +95,24 @@ const WaveTrack = () => {
     if (!wavesurfer) return;
     setIsPlaying(false);
 
-
-
     const hover = hoverRef.current!;
     const waveform = containerRef.current!;
+    waveform.addEventListener('pointermove', (e) => (hover.style.width = `${e.offsetX}px`))
 
-    waveform.addEventListener("pointermove", (e) => (hover.style.width = `${e.offsetX}px`));
+    waveform.addEventListener(
+      "pointermove",
+      (e) => (hover.style.width = `${e.offsetX}px`)
+    );
 
     const subscriptions = [
       wavesurfer.on("play", () => setIsPlaying(true)),
       wavesurfer.on("pause", () => setIsPlaying(false)),
-      wavesurfer.on("decode", (duration) => { setDuration(formatTime(duration)) }),
-      wavesurfer.on("timeupdate", (currentTime) => { setTime(formatTime(currentTime)) }),
+      wavesurfer.on("decode", (duration) => {
+        setDuration(formatTime(duration));
+      }),
+      wavesurfer.on("timeupdate", (currentTime) => {
+        setTime(formatTime(currentTime));
+      }),
     ];
     return () => {
       subscriptions.forEach((unsub) => unsub());
@@ -92,16 +126,165 @@ const WaveTrack = () => {
     }
   }, [wavesurfer]);
 
+  const arrComments = [
+    {
+      id: 1,
+      avatar: "http://localhost:8000/images/chill1.png",
+      moment: 10,
+      user: "username 1",
+      content: "just a comment1"
+    },
+    {
+      id: 2,
+      avatar: "http://localhost:8000/images/chill1.png",
+      moment: 30,
+      user: "username 2",
+      content: "just a comment3"
+    },
+    {
+      id: 3,
+      avatar: "http://localhost:8000/images/chill1.png",
+      moment: 50,
+      user: "username 3",
+      content: "just a comment3"
+    },
+  ]
+
+  const calLeft = (moment: number) => {
+    const hardCodeDuration = 199;
+    const percent = (moment / hardCodeDuration) * 100;
+    return `${percent}%`
+  }
+
   return (
-    <div style={{ marginTop: 70 }}>
-      <div ref={containerRef} className="wave-form-container">
-        <div className="time" >{time}</div>
-        <div className="duration" >{duration}</div>
-        <div ref={hoverRef} className="hover-wave"></div>
+    <div style={{ marginTop: 20 }}>
+      <div
+        style={{
+          display: "flex",
+          gap: 15,
+          padding: 20,
+          height: 400,
+          background:
+            "linear-gradient(135deg, rgb(106, 112, 67) 0%, rgb(11,15,20) 100%)",
+        }}
+      >
+        <div
+          className="left"
+          style={{
+            width: "75%",
+            height: "calc(100%-10px)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <div
+            className="info"
+            style={{
+              display: "flex",
+            }}
+          >
+            <div
+              onClick={onPlayClick}
+              style={{
+                borderRadius: "50%",
+                background: "#f50",
+                height: "50px",
+                width: "50px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                cursor: "pointer",
+              }}
+            >
+              {isPlaying === true ? (
+                <PauseIcon sx={{ fontSize: 30, color: "white" }} />
+              ) : (
+                <PlayArrowIcon sx={{ fontSize: 30, color: "white" }} />
+              )}
+            </div>
+            <div style={{ marginLeft: 20 }}>
+              <div
+                style={{
+                  padding: "0 5px",
+                  background: "#333",
+                  fontSize: 30,
+                  width: "fit-content",
+                  color: "white",
+                }}
+              >
+                Hai Long
+              </div>
+              <div
+                style={{
+                  padding: "0 5px",
+                  marginTop: 10,
+                  background: "#333",
+                  fontSize: 20,
+                  width: "fit-content",
+                  color: "white",
+                }}
+              >
+                wang
+              </div>
+            </div>
+          </div>
+
+          <div ref={containerRef} className="wave-form-container">
+            <div className="time">{time}</div>
+            <div className="duration">{duration}</div>
+            <div ref={hoverRef} className="hover-wave"></div>
+            <div className="overlay"
+              style={{
+                position: 'absolute',
+                height: '30px',
+                width: '100%',
+                bottom: '0',
+                backdropFilter: 'brightness(0.5)'
+              }}
+            ></div>
+            <div className="comments"
+              style={{ position: 'relative' }}
+            >
+
+              {arrComments.map(item => {
+                return (
+                  <Tooltip title={item.content} arrow color="yellow">
+                    <img
+                      onPointerMove={(e) => {
+                        const hover = hoverRef.current!;
+                        hover.style.width = calLeft(item.moment)
+
+                      }}
+                      key={item.id}
+                      src={`http://localhost:8000/images/chill1.png`} alt=""
+                      style={{ height: 20, width: 20, position: 'absolute', top: '71px', zIndex: 20, left: calLeft(item.moment) }}
+                    />
+                  </Tooltip>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="right"
+          style={{
+            width: '25%',
+            gap: '10px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <div style={
+            {
+              background: '#ccc',
+              height: '250px',
+              maxWidth: '250px',
+              width: '100%'
+            }
+          }></div>
+        </div>
       </div>
-      <Button onClick={onPlayClick}>
-        {wavesurfer?.isPlaying() === true ? "Pause" : "Play"}
-      </Button>
     </div>
   );
 };
