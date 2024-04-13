6ec0692f0dc26db9156f377ca286c518cb0783fe
HLong <hailongkc1203@gmail.com>
2024-04-13 18:21:22 +0700

    Nested layout with route group

--- src/components/header/app.header.tsx
+++ src/components/header/app.header.tsx
@@ -64,7 +64,6 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 export default function AppHeader() {
   const { data: session } = useSession();
-  console.log(">>>check session", session);
 
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
