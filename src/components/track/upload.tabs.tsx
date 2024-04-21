03ea4e24b559afaffe2e7d13a47b0a5951d365e2
HLong <hailongkc1203@gmail.com>
2024-04-21 17:04:18 +0700

    Design Tab Infomation

--- src/components/track/upload.tabs.tsx
+++ src/components/track/upload.tabs.tsx
@@ -35,39 +35,6 @@ const a11yProps = (index: number) => {
   };
 };
 
-const BasicTabs = () => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-        Item One
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
-      </CustomTabPanel>
-    </Box>
-  );
-};
-
 const UploadTabs = () => {
   const [value, setValue] = React.useState(0);
 
