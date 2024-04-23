6e52d1fd5c45db5c04cd38f6fbdf6ec5736ba422
HLong <hailongkc1203@gmail.com>
2024-04-23 21:06:23 +0700

    design card playlist profile

--- src/app/(user)/profile/[slug]/page.tsx
+++ src/app/(user)/profile/[slug]/page.tsx
@@ -1,17 +1,28 @@
+import ProfileTracks from "@/components/header/profile.tracks";
 import { sendRequest } from "@/utils/api";
+import { Container, Grid } from "@mui/material";
 import React from "react";
 
 const ProfilePage = async ({ params }: { params: { slug: string } }) => {
-  const tracks = await sendRequest<IBackendRes<ITrackTop[]>>({
+  const tracks = await sendRequest<IBackendRes<IModelPaginate<ITrackTop>>>({
     url: "http://localhost:8000/api/v1/tracks/users?current=1&pageSize=10",
     method: "POST",
     body: { id: params.slug },
   });
+  const data = tracks?.data?.result ?? [];
 
   return (
-    <>
-      <div>ProfilePage {params.slug}</div>
-    </>
+    <Container sx={{ my: 5 }}>
+      <Grid container spacing={5}>
+        {data.map((item: ITrackTop, index: number) => {
+          return (
+            <Grid item xs={12} md={6} key={index}>
+              <ProfileTracks data={item} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 };
 
---
+++ src/components/header/profile.tracks.tsx
@@ -0,0 +1,92 @@
+"use client";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+const ProfileTracks = (props: any) => {
+  const { data } = props;
+  const theme = useTheme();
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+  console.log(data);
+  return (
+    <Grid
+      container
+      rowSpacing={3}
+      columnSpacing={{ xs: 3, sm: 2, md: 3 }}
+      sx={{
+        display: "flex",
+      }}
+    >
+      <Grid item xs={12}>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
+              <Typography component="div" variant="h5">
+                {data.title}
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                component="div"
+              >
+                {data.description}
+              </Typography>
+            </CardContent>
+            <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+              <IconButton aria-label="previous">
+                {theme.direction === "rtl" ? (
+                  <SkipNextIcon />
+                ) : (
+                  <SkipPreviousIcon />
+                )}
+              </IconButton>
+              <IconButton aria-label="play/pause">
+                <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+              </IconButton>
+              <IconButton aria-label="next">
+                {theme.direction === "rtl" ? (
+                  <SkipPreviousIcon />
+                ) : (
+                  <SkipNextIcon />
+                )}
+              </IconButton>
+            </Box>
+          </Box>
+          <CardMedia
+            component="img"
+            sx={{ width: 151 }}
+            image={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/${data.imgUrl}`}
+            alt="Live from space album cover"
+          />
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ProfileTracks;
